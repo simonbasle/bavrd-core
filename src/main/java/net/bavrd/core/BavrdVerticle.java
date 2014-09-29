de80a803c4f9501d9ccd1e56e50487f9ac53bf11
Simon Baslé <simon.basle@gmail.com>
2014-09-29 17:17:17 +0200

    basic template for bavrd module : bavrdVerticle

---
+++ src/main/java/net/bavrd/core/BavrdVerticle.java
@@ -0,0 +1,44 @@
+package net.bavrd.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+public abstract class BavrdVerticle extends Verticle {
+
+  /**
+   * default init behavior for a BAVRD Verticle : register a handler for "bavrd-discover" events
+   */
+  @Override
+  public void start() {
+    vertx.eventBus().registerHandler("bavrd-discover", new Handler<Message<String>>() {
+      @Override
+      public void handle(Message<String> event) {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("moduleType", getType());
+        jsonMap.put("moduleHelp", getHelp());
+        event.reply(new JsonObject());
+      }
+    });
+
+    startBavrd();
+
+    container.logger().info(getName() + " BAVRD module started");
+  }
+
+  /** init method for the concrete BAVRD Verticle, other than bavrd-discover */
+  public abstract void startBavrd();
+
+  /** @return the name of the BAVRD module */
+  public abstract String getName();
+
+  /** @return the type of this module */
+  public abstract BavrdComponent getType();
+
+  /** @return a help String (that could be shown to a user) to describe what commands this module reacts to  */
+  public abstract String getHelp();
+}
