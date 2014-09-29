25427c58dbd8ff95850295c754183077455342be
Simon Baslé <simon.basle@gmail.com>
2014-09-29 21:03:47 +0200

    cleanup of vertx example files

--- src/main/resources/ping_verticle.py
+++
@@ -1,8 +0,0 @@
-import vertx
-from core.event_bus import EventBus
-
-def handler(msg):
-    msg.reply('pong!')
-    print 'sent back pong Python!'
-
-EventBus.register_handler('bavrd-incoming', handler=handler)
