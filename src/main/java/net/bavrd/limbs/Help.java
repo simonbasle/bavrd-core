5c3150496a19c0e01e9bd0d1976209f3c851b2ea
Simon Baslé <simon.basle@gmail.com>
2014-10-01 13:31:19 +0200

    added Help module that lists registered commands

---
+++ src/main/java/net/bavrd/limbs/Help.java
@@ -0,0 +1,50 @@
+package net.bavrd.limbs;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.shareddata.ConcurrentSharedMap;
+
+import net.bavrd.core.BavrdComponent;
+import net.bavrd.core.BavrdVerticle;
+import net.bavrd.core.EventEnum;
+import net.bavrd.core.Face;
+
+public class Help extends BavrdVerticle {
+
+  @Override
+  public void startBavrd() {
+    EventBus eventBus = vertx.eventBus().registerHandler(EventEnum.INCOMING.vertxEndpoint, new Handler<Message<JsonObject>>() {
+
+      @Override
+      public void handle(Message<JsonObject> m) {
+        Face.FaceMessage fm = Face.FaceMessage.decodeFrom(m.body());
+        if ("help".equalsIgnoreCase(fm.message)) {
+          ConcurrentSharedMap<String, String> helpMap = vertx.sharedData().getMap(SHARED_DATA_HELP);
+          StringBuffer help = new StringBuffer("The following commands are available for " + container.config().getString("botName") + ":");
+          for(Map.Entry<String, String> e : helpMap.entrySet()) {
+            help.append("\n*").append(e.getKey()).append("* - ").append(e.getValue());
+          }
+          Face.FaceMessage response = new Face.FaceMessage(Face.FaceMessage.SEND_TO_CHANNEL, fm.channel, help.toString());
+          vertx.eventBus().send(EventEnum.OUTGOING.vertxEndpoint, response.asJson());
+        }
+      }
+    });
+  }
+
+  @Override
+  public BavrdComponent getType() {
+    return BavrdComponent.LIMB;
+  }
+
+  @Override
+  public Map<String, String> getHelp() {
+    return Collections.singletonMap("help", "lists all registered commands in this bot");
+  }
+}
