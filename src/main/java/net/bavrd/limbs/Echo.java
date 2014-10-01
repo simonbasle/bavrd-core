810d253767eb5563b803cb1a17ccf0d865c81345
Simon Baslé <simon.basle@gmail.com>
2014-10-01 13:30:43 +0200

    added echo command (not same as say)

--- src/main/java/net/bavrd/limbs/Echo.java
+++ src/main/java/net/bavrd/limbs/Echo.java
@@ -1,6 +1,6 @@
 package net.bavrd.limbs;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,6 +17,7 @@ import net.bavrd.core.Face;
 public class Echo extends BavrdVerticle {
 
   private static final Pattern SAY_PATTERN = Pattern.compile("say (.*)");
+  private static final Pattern ECHO_PATTERN = Pattern.compile("echo (.*)");
 
   private static final String DEFAULT_SAY_FORMAT = "%u told me to say '%m'";
 
@@ -31,16 +32,24 @@ public class Echo extends BavrdVerticle {
       @Override
       public void handle(Message<JsonObject> m) {
         Face.FaceMessage fm = Face.FaceMessage.decodeFrom(m.body());
-        Matcher matcher = SAY_PATTERN.matcher(fm.message);
-        if (matcher.matches()) {
-          say(fm.channel, fm.user, matcher.group(1));
+        Matcher sayMatcher = SAY_PATTERN.matcher(fm.message);
+        if (sayMatcher.matches()) {
+          say(fm.channel, fm.user, sayMatcher.group(1), true);
+          return;
+        }
+
+        Matcher echoMatcher = ECHO_PATTERN.matcher(fm.message);
+        if (echoMatcher.matches()) {
+          say(fm.channel, fm.user, echoMatcher.group(1), false);
         }
       }
     });
   }
 
-  protected void say(String channel, String user, String params) {
-    String text = sayFormat.replaceAll("%u", user).replaceAll("%m", params);
+  protected void say(String channel, String user, String message, boolean wrapMessage) {
+    String text = message;
+    if (wrapMessage)
+      text = sayFormat.replaceAll("%u", user).replaceAll("%m", message);
     Face.FaceMessage reply = new Face.FaceMessage(Face.FaceMessage.SEND_TO_CHANNEL, channel, text);
     vertx.eventBus().send(EventEnum.OUTGOING.vertxEndpoint, reply.asJson());
   }
@@ -51,7 +60,10 @@ public class Echo extends BavrdVerticle {
   }
 
   @Override
-  public Map<String, String> getHelp() {
-    return Collections.singletonMap("say X", "will echo back X to the user");
+  public java.util.Map<String, String> getHelp() {
+    Map<String, String> help = new HashMap<>();
+    help.put("echo X", "will echo back X to the user");
+    help.put("say X", "will echo back X to the user, inside a phrase like \"user told me to say 'X'\"");
+    return help;
   }
 }
