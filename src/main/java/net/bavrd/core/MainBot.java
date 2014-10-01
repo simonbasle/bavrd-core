b7861dfbe50c5c0daa39ffd03e0ad850da3f6d7e
Simon Baslé <simon.basle@gmail.com>
2014-10-01 13:26:02 +0200

    mainbot generates a module's config with common configuration

--- src/main/java/net/bavrd/core/MainBot.java
+++ src/main/java/net/bavrd/core/MainBot.java
@@ -1,5 +1,8 @@
 package net.bavrd.core;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.json.JsonArray;
@@ -10,16 +13,29 @@ public class MainBot extends Verticle {
 
   @Override
   public void start() {
-    if (!container.config().containsField("modules"))
+    JsonObject globalConfig = container.config();
+    if (!globalConfig.containsField("modules"))
       throw new IllegalArgumentException("failed to load 'modules' configuration");
 
-    JsonArray mods = container.config().getArray("modules");
+    //this part of the config is global, will be appended to the config of each module
+    Set<String> commonFieldNames = new HashSet<>(globalConfig.getFieldNames());
+    commonFieldNames.remove("modules");
+
+    //the "modules" part of the config lists all the required bavrd modules
+    JsonArray mods = globalConfig.getArray("modules");
     for (Object m : mods) {
       JsonObject mod = (JsonObject) m;
       final String modName = mod.getString("moduleName");
-      JsonObject modConfig = mod.getObject("moduleConf");
       String modRef = mod.getString("moduleRef");
 
+      //reconstruct a proper module config with common config elements and the module declared name
+      JsonObject modConfig = mod.getObject("moduleConf").copy();
+      for (String f : commonFieldNames) {
+        modConfig.putValue(f, globalConfig.getField(f));
+      }
+      modConfig.putString("moduleName", modName);
+
+      //deploy module
       container.logger().info("Attempting to deploy "+ modName);
       container.deployVerticle(modRef, modConfig, new Handler<AsyncResult<String>>() {
         @Override
